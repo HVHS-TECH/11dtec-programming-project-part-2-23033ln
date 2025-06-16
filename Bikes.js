accae73f0423b92d192611020970aa8f8cd8df56
23033ln <23033ln@hvhs.school.nz>
2025-06-16 20:59:48 +0000

    Jun 16, 2025, 8:59 PM

---
+++ Bikes.js
@@ -0,0 +1,50 @@
+/****************************
+ for html <button onclick="rentBike();"> </button>
+ *     document.getElementById("myButton").addEventListener("click", myFunction);
+    function myFunction() {
+      alert("Button Clicked!");
+    }
+ Intronductions
+ Lukas
+29/04/25 
+Intro
+*****************************/
+console.log("Running Website");
+
+//variables
+var askUserName
+
+
+/**************************** 
+ *Main code
+ ****************************/ 
+function askUserName() {
+  prompt("What is you name?");
+  alert("Hello,") 
+  askQuestion();
+}
+function askQuestion(){
+askQuestion= prompt("Do you want to rent this bike?  Y for yes N for no");
+} if (askQuestion == "Y" ){
+askDays();
+} else   {
+
+}
+function askDays() {
+prompt("How long do you want to rent this for?/n 1 to 60 days only");
+} if (askDays  1 >||< 30 ){
+  alert("that will be");
+} else if (askdays == 30  60) {
+  
+}
+
+function rentBike(){
+askUserName();
+}
+
+    
+
+
+/****************************
+ funtions
+ ****************************/
